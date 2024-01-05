b2cd4cf039736108e0481b6594f74183d62ebbef
shrey0511 <112866424+shrey0511@users.noreply.github.com>
2024-01-05 15:37:24 +0530

    updated

--- src/pages/achievement.tsx
+++ src/pages/achievement.tsx
@@ -6,19 +6,19 @@ import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 600,
     slidesToShow: 4,
     slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1025,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
+          dots: false
         }
       },
       {
